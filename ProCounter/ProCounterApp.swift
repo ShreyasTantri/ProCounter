644abb455d2ece3e2c9f0e41885a17c2abd50266
ShreyasTantri <shreyastantrik29@gmail.com>
2026-01-03 21:32:43 +0530

    Initial Commit

---
+++ ProCounter/ProCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProCounterApp.swift
+//  ProCounter
+//
+//  Created by CCS038 on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ProCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
